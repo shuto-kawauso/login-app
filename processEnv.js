4ae66994e6e07e46a6a33a638d470e6a4b279743
Shuto Kikuchi <mizoren8@gmail.com>
2018-12-31 10:10:54 +0900

    Delete processEnv.js

--- processEnv.js
+++
@@ -1,9 +0,0 @@
-export default
-{
-  API_KEY: 'AIzaSyAWG8d36JFOtRWfkEcDTA2gwWkD_EdPW0Y',
-  AUTH_DOMAIN: 'login-app-9644d.firebaseapp.com',
-  DATABASE_URL: 'https://login-app-9644d.firebaseio.com',
-  PROJECT_ID: 'login-app-9644d',
-  STORAGE_BUCKET: 'login-app-9644d.appspot.com',
-  MESSAGING_SENDER_ID: '288214090031'
-}
